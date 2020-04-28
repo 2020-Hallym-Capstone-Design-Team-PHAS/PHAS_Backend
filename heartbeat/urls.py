c4a51435f3545ef85b82af3ea2120f9879e7fff9
ddingg <area409@gmail.com>
2020-04-28 22:11:47 +0900

    DONE audio file upload API

--- heartbeat/urls.py
+++ heartbeat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from heartbeat.views import *
 
 urlpatterns = [
-    path("save", saveAudioFile, name = "save Audio File")
+    path("save", save_audio_file, name = "save Audio File")
 
 ]
