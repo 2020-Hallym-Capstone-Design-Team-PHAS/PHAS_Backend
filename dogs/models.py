c522b13e9ac08bc2c5ec240971172dc681abd645
dudwh9615 <dudwh9615@naver.com>
2020-04-29 15:30:07 +0900

    Dog(app) 데이터베이스에 정보입력 구현

--- dogs/models.py
+++ dogs/models.py
@@ -2,10 +2,8 @@ from django.db import models
 import users.models as us
 # Create your models here.
 class DogInfo(models.Model):
-    user_id = models.ForeignKey(us.UserInfo, on_delete=models.CASCADE)
-    dog_idx = models.AutoField(primary_key=True)
-    dog_breed = models.CharField(max_length=64)
-    dog_name = models.CharField(max_length=32)
-    dog_size = models.PositiveSmallIntegerField()
-    dog_birth = models.DateTimeField()
-    create_date = models.DateField(auto_now_add=True)
+    user_id = models.ForeignKey('users.UserInfo', on_delete=models.CASCADE)
+    dog_breed = models.CharField(max_length=64, null=True)
+    dog_name = models.CharField(max_length=32, primary_key=True)
+    dog_size = models.PositiveSmallIntegerField(null=True)
+    dog_birth = models.DateField(null=True)
--- dogs/views.py
+++ dogs/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
+from dogs.models import DogInfo
+from .forms import DogForm
+from django.views.decorators.csrf import csrf_exempt
+from users.models import UserInfo as us
 # Create your views here.
 
+@csrf_exempt
 def dogRegist(request):
-    return HttpResponse("Rgst dog")
-
+    try:
+        if request.method == 'POST':
+            if DogInfo.objects.filter(Q(user_id=request.POST['user_id']) & Q(dog_name=request.POST['dog_name'])):
+                return HttpResponse("이미 있는 정보")
+            dogdata = DogInfo.objects.create(user_id = us.objects.get(user_id=request.POST['user_id']),
+                                             dog_name = request.POST['dog_name'],
+                                             dog_breed = request.POST['dog_breed'],
+                                             dog_size = request.POST['dog_size'],
+                                             dog_birth = request.POST['dog_birth'])
+            return HttpResponse("입력완료")
+            #if diform.is_valid():
+            #    diform.save()
+            #    return HttpResponse("입력 성공")#Http status 200
+        else:
+            #diform = DogForm
+            return HttpResponse("허용하지 않은 Http method 입니다.") #Http status 403
+    except Exception as e:
+        return HttpResponse(e, " : Error 입니다.")
 def dogInfo(request):
     return HttpResponse("dog info")
