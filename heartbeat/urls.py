ad26fb9fd5462c066ec39721f69d6a1dfe7ed5cb
조명근 <area409@gmail.com>
2020-05-04 23:14:39 +0900

    ADD heartbeat API DONE

--- heartbeat/urls.py
+++ heartbeat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from heartbeat.views import *
 
 urlpatterns = [
-    path("save", save_audio_file, name = "save Audio File")
-
+    path("save", save_audio_file, name = "save Audio File"),
+    path("search/log", search_log, name = "search Log")
 ]
--- heartbeat/views.py
+++ heartbeat/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from heartbeat.models import Heartbeat
 from django.http import *
-from datetime import datetime
+from heartbeat.models import Heartbeat
 from heartbeat.forms import HeartBeatForm
 from django.views.decorators.csrf import csrf_exempt
-from django.core.files.storage import FileSystemStorage
+import json
+from datetime import datetime
+
 
 
 @csrf_exempt
@@ -24,3 +24,28 @@ def save_audio_file(request):
             return HttpResponseNotAllowed()
     except Exception as e:
         return HttpResponseServerError(e)
+
+@csrf_exempt
+def search_log(request):
+    try:
+        if request.method == "POST":
+            json_data = json.loads(request.body)
+            audio_info = Heartbeat.objects.filter(audio_idx__exact=json_data['audio_idx'])[0]
+            audio_info_dic = {
+                              "audio_idx":audio_info.audio_idx,
+                              "dog_name":audio_info.dog_name,
+                              "user_id" : audio_info.user_id,
+                              "create_data":json_default(audio_info.create_date),
+                              "heartbeat_normal_condition":audio_info.heartbeat_normal_condition
+                              }
+            return HttpResponse(json.dumps(audio_info_dic))
+        else:
+            return HttpResponseForbidden()
+    except Exception as e:
+        return HttpResponseServerError(e)
+
+
+def json_default(value):
+    if isinstance(value, datetime):
+        return value.strftime('%Y-%m-%d_%H:%M')
+
