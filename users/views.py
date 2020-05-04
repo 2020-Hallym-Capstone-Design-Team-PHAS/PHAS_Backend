07e3169c12b74369999b42e57ba2312754aea830
조명근 <area409@gmail.com>
2020-05-05 00:02:26 +0900

    Edit regist

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseForbidden, HttpResponseServerError, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from users.forms import *
-from django.utils.encoding import smart_str
+from datetime import date
 import bcrypt
 import json
 
@@ -14,13 +14,16 @@ def userRegist(request):
         if request.method == 'POST':
             json_data = json.loads(request.body)
             hashed_password = bcrypt.hashpw(json_data['user_password'].encode('utf-8'), bcrypt.gensalt()).decode()
-            user_data = UserInfo(user_name=json_data['user_name'], user_password=hashed_password, user_id=json_data['user_id'])
+            user_data = UserInfo(user_name=json_data['user_name'], user_password=hashed_password, user_id=json_data['user_id'], create_date=date.today())
             user_data.save()
             return HttpResponse()
         else:
             return HttpResponseNotAllowed("허용하지 않은 Http method 입니다.") #Http status 403
-    except Exception as e:
-        return HttpResponseServerError(user_data)
+    except UnboundLocalError as unbound:
+        message = {"message":"Parameter not vaild."}
+        return HttpResponseBadRequest(message)
+    except Exception as ex:
+        return HttpResponseServerError(ex)
 
 
 @csrf_exempt
