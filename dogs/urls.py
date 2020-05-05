d3a2962a5088621c668660502dcb58caf58a47f3
dudwh9615 <dudwh9615@naver.com>
2020-05-06 01:13:49 +0900

    Dog(app) 데이터 입/출력 json포맷으로 변경 애완동물 정보 삭제 구현

--- dogs/urls.py
+++ dogs/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('dogregist', dogRegist, name = 'dog_regist'),
     path('doginfo_user', dogInfo_user, name = 'dog_info_user'),
     path('doginfo_dog', dogInfo_dog, name = 'dog_info_dog'),
+    path('doginfo_del', dogInfo_del, name = 'dog_info_del'),
+
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -5,39 +5,41 @@ from dogs.models import DogInfo
 from .forms import DogForm
 from django.views.decorators.csrf import csrf_exempt
 from users.models import UserInfo as us
+import json
 # Create your views here.
 
 @csrf_exempt
 def dogRegist(request):#강아지 정보 등록
     try:
         if request.method == 'POST':
-            if DogInfo.objects.filter(Q(user_id=request.POST['user_id']) & Q(dog_name=request.POST['dog_name'])):
+            dog_json = json.loads(request.body)
+            if DogInfo.objects.filter(Q(user_id=dog_json['user_id']) & Q(dog_name=dog_json['dog_name'])):
                 return HttpResponse("이미 있는 정보")
-            dogdata = DogInfo.objects.create(user_id = us.objects.get(user_id=request.POST['user_id']),
-                                             dog_name = request.POST['dog_name'],
-                                             dog_breed = request.POST['dog_breed'],
-                                             dog_size = request.POST['dog_size'],
-                                             dog_birth = request.POST['dog_birth'])
-            return HttpResponse("입력완료")
-            #if diform.is_valid():
-            #    diform.save()
-            #    return HttpResponse("입력 성공")#Http status 200
+
+            dogdata = DogInfo.objects.create(user_id = us.objects.get(user_id=dog_json['user_id']),
+                                             dog_name = dog_json['dog_name'],
+                                             dog_breed = dog_json['dog_breed'],
+                                             dog_size = dog_json['dog_size'],
+                                             dog_birth = dog_json['dog_birth'])
+            result_data = {"result_code" : 1}
+            return HttpResponse(json.dumps(result_data))
         else:
-            #diform = DogForm
             return HttpResponse("허용하지 않은 Http method 입니다.") #Http status 403
     except Exception as e:
         return HttpResponse(e, " : Error 입니다.")
+
 @csrf_exempt
 def dogInfo_user(request):#사용자 보유 애완동물 목록
     try:
         if request.method == 'POST':
-            s_data = DogInfo.objects.filter(Q(user_id=request.POST['user_id']))
+            dog_json = json.loads(request.body)
+            s_data = DogInfo.objects.filter(Q(user_id=dog_json['user_id']))
             if s_data:
+
                 return HttpResponse(s_data.values('dog_name'))
             else:
-                return HttpResponse("해당 사용자 애완동물 정보 없음")
+                return HttpResponse(json.dumps({"result_code" : 0}))
         else:
-            # diform = DogForm
             return HttpResponse("허용하지 않은 Http method 입니다.")  # Http status 403
     except e:
         return HttpResponse(e, " : Error 입니다.")
@@ -46,9 +48,28 @@ def dogInfo_user(request):#사용자 보유 애완동물 목록
 def dogInfo_dog(request):#사용자 보유 애완동물 상세정보
     try:
         if request.method == 'POST':
-            s_data = DogInfo.objects.filter(Q(user_id=request.POST['user_id']) & Q(dog_name=request.POST['dog_name']))
+            dog_json = json.loads(request.body)
+            s_data = DogInfo.objects.filter(Q(user_id=dog_json['user_id']) & Q(dog_name=dog_json['dog_name']))
             if s_data:
                 return HttpResponse(s_data.values())
+            else:
+                return HttpResponse("해당 애완동물 정보 없음")
+        else:
+            # diform = DogForm
+            return HttpResponse("허용하지 않은 Http method 입니다.")  # Http status 403
+    except e:
+        return HttpResponse(e, " : Error 입니다.")
+
+@csrf_exempt
+def dogInfo_del(request):#사용자 보유 애완동물 상세정보
+    try:
+        if request.method == 'POST':
+            dog_json = json.loads(request.body)
+
+            s_data = DogInfo.objects.filter(Q(user_id=dog_json['user_id']) & Q(dog_name=dog_json['dog_name']))
+            if s_data:
+                s_data.delete()
+                return HttpResponse('삭제 완료')
             else:
                 return HttpResponse("해당 애완동물 정보 없음")
         else:
