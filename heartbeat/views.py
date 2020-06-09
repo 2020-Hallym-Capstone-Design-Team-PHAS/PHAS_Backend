54a5d5df45c653bfe7e39262e6b1fbcf3c5d90ba
ddingg <area409@gmail.com>
2020-06-09 06:31:26 +0000

    done noise Reduce process.

--- heartbeat/views.py
+++ heartbeat/views.py
@@ -5,35 +5,45 @@ from django.views.decorators.csrf import csrf_exempt
 from heartbeat.PhasNoiseReduce import noiseReduce
 import json
 from datetime import datetime
+import logging
+import os
+import sys
+import time
 
 @csrf_exempt
 def save_audio_file(request):
     try:
         if request.method == "POST":
-            audio_data = HeartBeatForm(request.POST, request.FILES)
 
-            if audio_data.is_valid() and request.FILES['audio_file']:
+            audio_data = HeartBeatForm(request.POST, request.FILES)
+            
+
+            if audio_data.is_valid():
                 file_name_rebase = audio_data.cleaned_data['user_id'] + \
                                    '_' + audio_data.cleaned_data['dog_name'] + \
                                    '_' + datetime.now().strftime("%Y-%m-%d_%H시:%M분") + \
-                                   '.wav'
-
+                                   '.aac'
+                
                 obj = Heartbeat(user_id=audio_data.cleaned_data['user_id'],
                                 dog_name=audio_data.cleaned_data['dog_name'])
                 obj.audio_file = request.FILES['audio_file']
                 obj.audio_file.name = file_name_rebase
                 obj.heartbeat_normal_condition = -1
                 obj.save()
-
+                
+                os.system('ffmpeg -i ./heartbeat_data/' + obj.audio_file.name + ' ' + './heartbeat_data/' + obj.audio_file.name[:-4] + '.wav')
+                os.system('rm -r ./heartbeat_data/'+ obj.audio_file.name)
+                
                 noiseReduce(obj.audio_file.name[:-4])
 
                 return HttpResponse()
             else:
-                return HttpResponseForbidden(audio_data)
+                return HttpResponseForbidden(request.FILES['audio_file'])
         else:
             return HttpResponseNotAllowed()
     except Exception as e:
         return HttpResponseServerError(e)
+
 
 @csrf_exempt
 def search_log(request):
